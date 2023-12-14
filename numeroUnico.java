9706d590f7d395075aed263231411c32fe4fcb11
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2023-12-14 10:29:13 -0300

    Update numeroUnico.java
    está falhando ainda...

--- numeroUnico.java
+++ numeroUnico.java
@@ -1,38 +1,50 @@
 public class Main
 {
     public static int numeroUnico(int [] input){
-        
-        int unico = 0;
-        
-        int [] copia = new int[input.length];
-        
+
+        int unico = 0; //reserva uma variável para pegar o elemento unico
+
+        int [] copia = new int[input.length];//e uma array copia para comparar
+
         for(int i = 0 ; i < input.length; i++){
-            copia[i] = input[i];
+            copia[i] = input[i];//copia a array principal
         }
-        
+
+        //compara: se der igual, zera o valor do índice
         for(int i=0; i<input.length; i++){
             unico = input[i];
-            for(int j=i+1; j<copia.length;j++){
+            for(int j=copia.length-1; j<i;j--){
                 if(copia[j]==unico)
-                    copia.remove(copia[j]);
+                    copia[j] = 0;
             }
         }
         
-        for (int elemento:copia){
-                unico = elemento;
+        /*//compara: se der igual, zera o valor do índice
+        for(int i=0; i<input.length; i++){
+            unico = input[i];
+            for(int j=copia.length-1; j>=0;j--){
+                if(copia[j]==unico)
+                    copia[j] = 0;
             }
-                
-        
-        if(copia==null)
-            return -1;
-        else
+        }
+        */
+        //procura o índice que não zerou, e guarda na variável único
+        for (int i=0; i<copia.length; i++){
+            if(copia[i]!=0)
+                unico = copia[i];
+            }
+
+        //exibe a variavel único se não for zero, caso contrário, retorna -1
+        if(unico!=0)
             return unico;
-           
+        else
+            return -1;
+
     }
 
-    
+
 	public static void main(String[] args) {
-		int lista[] = {2,1,2,3,1};
+		int lista[] = {101,10,20,30,40,50,50,40,30,20,101};
 		System.out.println(numeroUnico(lista));
 	}
 
